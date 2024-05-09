b04185f03a43f32acb8235f81e45b613775843d0
ryo <same20021108@gmail.com>
2024-05-09 20:23:11 +0800

    build: route setting

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello')  
-})
 
-app.listen(port, () => {
+// 解析 application/x-www-form-urlencoded
+  app.use(express.urlencoded({ extended: true }));
+// 解析 application/json
+  app.use(express.json());
+
+
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
+
+  app.post('/', (req, res) => {
+
+  })
+
+  app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
-})
+  })
